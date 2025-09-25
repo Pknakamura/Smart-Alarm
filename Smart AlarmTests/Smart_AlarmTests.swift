71e2977da244bf0c62c1b30c4fe02044e960e63f
Phillip Nakamura <pknakamura818@gmail.com>
2025-09-24 20:34:09 -0500

    Initial Commit

---
+++ Smart AlarmTests/Smart_AlarmTests.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_AlarmTests.swift
+//  Smart AlarmTests
+//
+//  Created by Phillip Nakamura on 9/24/25.
+//
+
+import Testing
+@testable import Smart_Alarm
+
+struct Smart_AlarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
